c9e0adc4d67316e8fb0c8d83849d70e2f77c8e0e
tedmdelacruz <tedmdelacruz@gmailcom>
2020-09-13 09:40:03 +0800

    Revise enum strategy to prevent memory issues

--- modules/amass.py
+++ modules/amass.py
@@ -9,26 +9,27 @@ class Amass(object):
     subdomains_file = "subdomains.txt"
 
     def __init__(self, domains, **kwargs):
-        self.domains = ",".join(domains)
+        self.domains = domains
+        self.domains_list = ",".join(domains)
         self.target_dir = kwargs.get("target_dir")
         self.config = kwargs.get("config")
         self.amass_dir = kwargs.get("amass_dir")
 
         with open(os.path.join(self.target_dir, self.domains_file), "w+") as f:
-            f.writelines(domains)
+            f.writelines(self.domains)
 
     def enum_subdomains(self):
-        subdomains_file = os.path.join(self.target_dir, self.subdomains_file)
-        cmd = ["amass", "enum", "-d", self.domains, "-dir", self.amass_dir, "-o", subdomains_file]
-        if self.config:
-            cmd = cmd + ["-config", self.config]
-        subprocess.run(cmd, check=True)
+        for domain in self.domains:
+            cmd = ["amass", "enum", "-d", domain, "-dir", self.amass_dir]
+            if self.config:
+                cmd = cmd + ["-config", self.config]
+            subprocess.run(cmd, check=True)
 
     def write_subdomains(self):
-        cmd = ["amass", "db", "-names", "-d", self.domains, "-dir", self.amass_dir]
+        cmd = ["amass", "db", "-names", "-d", self.domains_list, "-dir", self.amass_dir]
         with open(os.path.join(self.target_dir, self.subdomains_file), "w+") as f:
             subprocess.run(cmd, check=True, stdout=f)
 
     def track_subdomains(self):
-        cmd = ["amass", "track", "-last", "2", "-d", self.domains, "-dir", self.amass_dir]
+        cmd = ["amass", "track", "-last", "2", "-d", self.domains_list, "-dir", self.amass_dir]
         subprocess.run(cmd, check=True, capture_output=True)
