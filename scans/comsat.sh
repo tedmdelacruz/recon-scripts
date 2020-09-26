03e53dfa868ab77ae4f60dca3c0360aecc6b65e5
tedmdelacruz <tedmathewdelacruz@gmail.com>
2020-09-26 21:38:33 +0800

    Add cloud bucket enum to comsat.sh

--- scans/comsat.sh
+++ scans/comsat.sh
@@ -17,6 +17,7 @@ for target in $SELECTED_TARGETS; do
 
     enumerate_subdomains $target_dir
     probe_subdomains $target_dir
+    cloud_bucket_enum $target_dir
     git_scan $target_dir
     take_screenshots $target_dir
     notify_general "Done running comsat on target: $target"
